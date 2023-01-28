efb712f51d18f9fbd3d1eda4468b61db7720014d
HBMSRoboticsClub <103683123+HBMSRoboticsClub@users.noreply.github.com>
2023-01-28 16:56:42 -0500

    Pom-pom code beta v0.1.0

---
+++ pompomidcv0.1.0.c
@@ -0,0 +1,46 @@
+#include <kipr/wombat.h>
+//starting position 1000
+//left 1400
+//right 700
+int main()
+   
+{
+    printf("Hello World\n");
+    enable_servos();
+    set_servo_position(0,1000);
+    msleep(1000);
+    set_servo_position(0,640);
+    msleep(1000);
+    motor(0,50);
+    motor(3,50);
+    msleep(1500);
+    ao();
+    set_servo_position(0,1400);
+    
+   
+    msleep(1000);
+    set_servo_position(0,700);
+    msleep(1000);
+    set_servo_position(0,1400);
+    
+    
+    msleep(1000);
+    set_servo_position(0,700);
+    msleep(1000);
+    set_servo_position(0,1400);
+    
+    
+    msleep(1000);
+    set_servo_position(0,700);
+    msleep(1000);
+    set_servo_position(0,1400);
+    msleep(1000);
+    set_servo_position(0,1000);
+    msleep(1000);
+   
+    
+    
+    
+    
+    return 0;
+}
